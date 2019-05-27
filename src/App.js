42cfd36108951cc1d696821bcf8b79e5261afb7d
jenna <jenna_kim1@hotmail.com>
2019-05-27 13:49:47 +0200

    created Login.js and created card in it

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Login />
+      {/* <Dashboard /> */}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div class="card card-nav-tabs text-center">
+      <div class="card-header card-header-primary">
+        Log in
+      </div>
+      <div class="card-body">
+        <h4 class="card-title">Special title treatment</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#0" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-footer text-muted">
+        2 days ago
+      </div>
+  </div>
+  );
+}
+
+export default Login;
