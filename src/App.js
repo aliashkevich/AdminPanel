1622c7284a2de9bc7f70ec5474ac1efe5338fca7
jenna <jenna_kim1@hotmail.com>
2019-05-29 10:30:22 +0200

    added function and state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Login />
       {/* <Dashboard /> */}
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,77 @@
-import React from "react";
+import React, {Component} from 'react';
+import User from './User.json';
 
-function Login() {
-  return (
-    <div className="card card-nav-tabs text-center w-50 p-3 mx-auto">
-      <div className="card-header card-header-primary">
-        Log in
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleInputChange = event => {
+    const {value, name} = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    alert('Authentication coming soon!');
+  };
+
+  render() {
+    return (
+      <div className='card card-nav-tabs text-center w-50 p-3 mx-auto'>
+        <div className='card-header card-header-primary'>Log in</div>
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group mt-5'>
+            <label for='exampleInputEmail1'>Email address</label>
+            <input
+              type='email'
+              name='email'
+              value={this.state.email}
+              onChange={this.handleInputChange}
+              class='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+              placeholder='Enter email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label for='exampleInputPassword1'>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              className='form-control'
+              id='exampleInputPassword1'
+              placeholder='Password'
+              required
+            />
+          </div>
+          <div className='form-check'>
+            <label className='form-check-label'>
+              <input className='form-check-input' type='checkbox' value='' />
+              Remember next time
+              <span className='form-check-sign'>
+                <span className='check' />
+              </span>
+            </label>
+          </div>
+          <input type='submit' value='submit' className='btn btn-primary' />
+        </form>
       </div>
-      <form>
-        <div className="form-group mt-5">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-        </div>
-        <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-        </div>
-        <div className="form-check">
-          <label className="form-check-label">
-            <input className="form-check-input" type="checkbox" value="" />
-            Remember next time
-            <span className="form-check-sign">
-              <span className="check"></span>
-            </span>
-          </label>
-       </div>
-       <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-  </div>
-  );
+    );
+  }
 }
 
 export default Login;
