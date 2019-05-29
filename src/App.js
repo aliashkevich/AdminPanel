9fd5944ace046ed538cca6c5ab00cf370ae9d871
laura-mso <laura.socha@gmx.net>
2019-05-29 12:00:32 +0200

    Added more example pages and adjusted Table.js to jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard';
+import Dashboard from './examples/Dashboard';
+import Tables from './examples/Tables';
 import Home from './pages/Home';
 import Header from './components/Header';
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/dashboard' component={Dashboard} />
+          <Route path='/tables' component={Tables} />
         </Switch>
       </div>
     </BrowserRouter>
