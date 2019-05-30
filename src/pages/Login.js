ce2d9ac07663b2f4c3888a7b9406281b469aea90
jenna <jenna_kim1@hotmail.com>
2019-05-30 19:26:48 +0200

    edited catch error console.log

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -54,7 +54,7 @@ class Login extends Component {
           users: data.users,
         });
       })
-      .catch(error => console.log('error api'));
+      .catch(error => console.log(error));
   }
 
   render() {
