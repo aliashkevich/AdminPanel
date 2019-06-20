a3fb8570c4911827383ca3ee3a0eb2b6e01bc9fa
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-06-20 13:18:22 +0200

    fixed main panel and content wrap on routing

--- src/components/Project/ProjectNew.js
+++ src/components/Project/ProjectNew.js
@@ -148,147 +148,141 @@ class AddNewProject extends React.Component {
     });
     console.log(this.state);
     return (
-      <div className='main-panel'>
-        <div className='content'>
-          <div className='container-fluid'>
-            <div className='col-md-12'>
-              <div className='card'>
-                <div className='card-header card-header-info'>
-                  <h4 className='card-title'>New Project</h4>
+      <div className='container-fluid'>
+        <div className='col-md-12'>
+          <div className='card'>
+            <div className='card-header card-header-info'>
+              <h4 className='card-title'>New Project</h4>
+            </div>
+            <div className='card-body'>
+              <br />
+              <form onSubmit={this.handleSubmit}>
+                <div className='form-row'>
+                  <div
+                    className='form-group col-sm-12 col-md-6 has-info'
+                    style={{marginTop: '15px'}}>
+                    <label for='inputTitle'>Title:</label>
+                    <input
+                      style={{marginTop: '17px'}}
+                      type='text'
+                      name='title'
+                      className='form-control'
+                      id='inputTitle'
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                  <div
+                    className='col-sm-12 col-md-6 has-info'
+                    style={styles.clients}>
+                    <label for='inputClient' className='text-info'>
+                      Client:
+                    </label>
+                    <Select
+                      id='inputClient'
+                      name='clientSelect'
+                      value={this.state.clientSelect}
+                      options={clientOptions}
+                      onChange={this.handleClientChange}
+                      styles={styles.control}
+                      theme={theme => ({
+                        ...theme,
+                        borderRadius: 0,
+                        colors: {
+                          ...theme.colors,
+                          primary25: '#1fbfd7',
+                          primary: '#1fbfd7',
+                        },
+                      })}
+                      required
+                    />
+                  </div>
                 </div>
-                <div className='card-body'>
-                  <br />
-                  <form onSubmit={this.handleSubmit}>
-                    <div className='form-row'>
-                      <div
-                        className='form-group col-sm-12 col-md-6 has-info'
-                        style={{marginTop: '15px'}}>
-                        <label for='inputTitle'>Title:</label>
-                        <input
-                          style={{marginTop: '17px'}}
-                          type='text'
-                          name='title'
-                          className='form-control'
-                          id='inputTitle'
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </div>
-                      <div
-                        className='col-sm-12 col-md-6 has-info'
-                        style={styles.clients}>
-                        <label for='inputClient' className='text-info'>
-                          Client:
-                        </label>
-                        <Select
-                          id='inputClient'
-                          name='clientSelect'
-                          value={this.state.clientSelect}
-                          options={clientOptions}
-                          onChange={this.handleClientChange}
-                          styles={styles.control}
-                          theme={theme => ({
-                            ...theme,
-                            borderRadius: 0,
-                            colors: {
-                              ...theme.colors,
-                              primary25: '#1fbfd7',
-                              primary: '#1fbfd7',
-                            },
-                          })}
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className='form-row'>
-                      <div
-                        className='form-group col-sm-12 col-md-3 has-info'
-                        style={styles.dates}>
-                        <label for='inputStartDate'>Start Date:</label>
-                        <input
-                          style={{marginTop: '17px'}}
-                          type='date'
-                          name='start_date'
-                          className='form-control'
-                          id='inputStartDate'
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </div>
-                      <div
-                        className='form-group col-sm-12 col-md-3 has-info'
-                        style={styles.dates}>
-                        <label for='inputEndDate'>Start End:</label>
-                        <input
-                          style={{marginTop: '17px'}}
-                          type='date'
-                          name='end_date'
-                          className='form-control'
-                          id='inputEndDate'
-                          onChange={this.handleChange}
-                          required
-                        />
-                      </div>
-                      <div
-                        className='col-sm-12 col-md-6 has-info'
-                        style={styles.participants}>
-                        <label for='inputParticipants' className='text-info'>
-                          Participants:
-                        </label>
-                        <Select
-                          id='inputParticipants'
-                          name='participantSelect'
-                          value={this.state.participantSelect}
-                          options={participantOptions}
-                          onChange={this.handleParticipantChange}
-                          isMulti
-                          styles={styles.control}
-                          theme={theme => ({
-                            ...theme,
-                            borderRadius: 0,
-                            colors: {
-                              ...theme.colors,
-                              primary25: '#1fbfd7',
-                              primary: 'black',
-                            },
-                          })}
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className='form-row'>
-                      <div className='form-group col-sm-12 col-md-12 has-info'>
-                        <label for='inputSummary'>Summary:</label>
-                        <textarea
-                          type='text'
-                          name='summary'
-                          className='form-control'
-                          id='inputSummary'
-                          onChange={this.handleChange}
-                          rows={10}
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className='form-row'>
-                      <div className=' form-group col-xs-1'>
-                        <Link to='/projects'>
-                          <button type='reset' className='btn btn-danger'>
-                            Cancel
-                          </button>
-                        </Link>
-                      </div>
-                      <div className='form-group col-xs-1 text-end ml-auto'>
-                        <button
-                          type='submit'
-                          className='btn btn-success btn-right'>
-                          Add
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                <div className='form-row'>
+                  <div
+                    className='form-group col-sm-12 col-md-3 has-info'
+                    style={styles.dates}>
+                    <label for='inputStartDate'>Start Date:</label>
+                    <input
+                      style={{marginTop: '17px'}}
+                      type='date'
+                      name='start_date'
+                      className='form-control'
+                      id='inputStartDate'
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                  <div
+                    className='form-group col-sm-12 col-md-3 has-info'
+                    style={styles.dates}>
+                    <label for='inputEndDate'>Start End:</label>
+                    <input
+                      style={{marginTop: '17px'}}
+                      type='date'
+                      name='end_date'
+                      className='form-control'
+                      id='inputEndDate'
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                  <div
+                    className='col-sm-12 col-md-6 has-info'
+                    style={styles.participants}>
+                    <label for='inputParticipants' className='text-info'>
+                      Participants:
+                    </label>
+                    <Select
+                      id='inputParticipants'
+                      name='participantSelect'
+                      value={this.state.participantSelect}
+                      options={participantOptions}
+                      onChange={this.handleParticipantChange}
+                      isMulti
+                      styles={styles.control}
+                      theme={theme => ({
+                        ...theme,
+                        borderRadius: 0,
+                        colors: {
+                          ...theme.colors,
+                          primary25: '#1fbfd7',
+                          primary: 'black',
+                        },
+                      })}
+                      required
+                    />
+                  </div>
                 </div>
-              </div>
+                <div className='form-row'>
+                  <div className='form-group col-sm-12 col-md-12 has-info'>
+                    <label for='inputSummary'>Summary:</label>
+                    <textarea
+                      type='text'
+                      name='summary'
+                      className='form-control'
+                      id='inputSummary'
+                      onChange={this.handleChange}
+                      rows={10}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className='form-row'>
+                  <div className=' form-group col-xs-1'>
+                    <Link to='/projects'>
+                      <button type='reset' className='btn btn-danger'>
+                        Cancel
+                      </button>
+                    </Link>
+                  </div>
+                  <div className='form-group col-xs-1 text-end ml-auto'>
+                    <button type='submit' className='btn btn-success btn-right'>
+                      Add
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
--- src/pages/NewProject.js
+++ src/pages/NewProject.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
-import AddNewProject from '../components/Project/ProjectNew';
+import ProjectNew from '../components/Project/ProjectNew';
 
 export default function NewProject() {
   return (
     <React.Fragment>
       <Header />
-      <AddNewProject />
+      <div className='main-panel'>
+        <div className='content'>
+          <ProjectNew />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
