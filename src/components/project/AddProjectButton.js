8312a8af747b3490381d2ce10a5ec4fbaed78c82
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-06-21 11:50:19 +0200

    Fix after merge

--- src/components/project/AddProjectButton.js
+++ src/components/project/AddProjectButton.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import AddButton from '../global/AddButton';
+import {Link} from 'react-router-dom';
 
 export default function AddProjectButton() {
   const buttonName = 'New Project';
-  return <AddButton buttonName={buttonName} />;
+  return (
+    <React.Fragment>
+      <Link className='text-light' to='/projects/new'>
+        <AddButton buttonName={buttonName} />
+      </Link>
+    </React.Fragment>
+  );
 }
