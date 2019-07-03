68e46d13cc5755384fca8c94b3c92e82da6e8541
jenna <jenna_kim1@hotmail.com>
2019-07-03 18:33:54 +0200

    display task summary in task table

--- src/components/project/Tasks.js
+++ src/components/project/Tasks.js
@@ -1,133 +1,31 @@
 import React from 'react';
 
-function MemberTab() {
+function MemberTab(props) {
   return (
     <li className='nav-item'>
-      <a className='nav-link' href='#test' data-toggle='tab'>
-        member3
+      <a className='nav-link' href='#name1' data-toggle='tab'>
+        participant name
         <div className='ripple-container' />
       </a>
     </li>
   );
 }
 
-function MemberTask() {
+function MemberTask(props) {
   return (
-    <div className='tab-pane active' id='profile'>
+    <div className='tab-pane active' id='name1'>
       <table className='table'>
         <tbody>
-          <tr>
-            <td>
-              <div className='form-check'>
-                <label className='form-check-label'>
-                  <input
-                    className='form-check-input'
-                    type='checkbox'
-                    value=''
-                  />
-                  <span className='form-check-sign'>
-                    <span className='check' />
-                  </span>
-                </label>
-              </div>
-            </td>
-            <td>
-              Lines From Great Russian Literature? Or E-mails From My Boss?
-            </td>
-            <td className='td-actions text-right'>
-              <button
-                type='button'
-                rel='tooltip'
-                title='Edit Task'
-                className='btn btn-primary btn-link btn-sm'>
-                <i className='material-icons'>edit</i>
-              </button>
-              <button
-                type='button'
-                rel='tooltip'
-                title='Remove'
-                className='btn btn-danger btn-link btn-sm'>
-                <i className='material-icons'>close</i>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className='form-check'>
-                <label className='form-check-label'>
-                  <input
-                    className='form-check-input'
-                    type='checkbox'
-                    value=''
-                  />
-                  <span className='form-check-sign'>
-                    <span className='check' />
-                  </span>
-                </label>
-              </div>
-            </td>
-            <td>
-              Flooded: One year later, assessing what was lost and what was
-              found when a ravaging rain swept through metro Detroit
-            </td>
-            <td className='td-actions text-right'>
-              <button
-                type='button'
-                rel='tooltip'
-                title='Edit Task'
-                className='btn btn-primary btn-link btn-sm'>
-                <i className='material-icons'>edit</i>
-              </button>
-              <button
-                type='button'
-                rel='tooltip'
-                title='Remove'
-                className='btn btn-danger btn-link btn-sm'>
-                <i className='material-icons'>close</i>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className='form-check'>
-                <label className='form-check-label'>
-                  <input
-                    className='form-check-input'
-                    type='checkbox'
-                    value=''
-                    checked
-                  />
-                  <span className='form-check-sign'>
-                    <span className='check' />
-                  </span>
-                </label>
-              </div>
-            </td>
-            <td>Create 4 Invisible User Experiences you Never Knew About</td>
-            <td className='td-actions text-right'>
-              <button
-                type='button'
-                rel='tooltip'
-                title='Edit Task'
-                className='btn btn-primary btn-link btn-sm'>
-                <i className='material-icons'>edit</i>
-              </button>
-              <button
-                type='button'
-                rel='tooltip'
-                title='Remove'
-                className='btn btn-danger btn-link btn-sm'>
-                <i className='material-icons'>close</i>
-              </button>
-            </td>
-          </tr>
+          {props.projectTasks.map(task => {
+            return <TaskRow task={task} />;
+          })}
         </tbody>
       </table>
     </div>
   );
 }
 
-function TaskRow() {
+function TaskRow(props) {
   return (
     <tr>
       <td>
@@ -145,7 +43,7 @@ function TaskRow() {
           </label>
         </div>
       </td>
-      <td>Sign contract for "What are conference organizers afraid of?"</td>
+      <td>{props.task.summary}</td>
       <td className='td-actions text-right'>
         <button
           type='button'
@@ -201,226 +99,29 @@ export default class Tasks extends React.Component {
     const tasks = this.state.tasks;
     if (this.state.loading == false) {
       const projectTasks = tasks.filter(task => task.project_id === projectId);
-      console.log('projectTasks', projectTasks);
-    }
-    return (
-      <div className='col-lg-8 col-md-12 col-sm-12'>
-        <div className='card'>
-          <div className='card-header card-header-tabs card-header-info'>
-            <div className='nav-tabs-navigation'>
-              <div className='nav-tabs-wrapper'>
-                <span className='nav-tabs-title'>Tasks:</span>
-                <ul className='nav nav-tabs' data-tabs='tabs'>
-                  <MemberTab />
-                  <MemberTab />
-                  <MemberTab />
-                </ul>
+      return (
+        <div className='col-lg-8 col-md-12 col-sm-12'>
+          <div className='card'>
+            <div className='card-header card-header-tabs card-header-info'>
+              <div className='nav-tabs-navigation'>
+                <div className='nav-tabs-wrapper'>
+                  <span className='nav-tabs-title'>Tasks:</span>
+                  <ul className='nav nav-tabs' data-tabs='tabs'>
+                    <MemberTab />
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='card-body'>
-            <div className='tab-content'>
-              <div className='tab-pane' id='test'>
-                <table className='table'>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <div className='form-check'>
-                          <label className='form-check-label'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              value=''
-                              checked
-                            />
-                            <span className='form-check-sign'>
-                              <span className='check' />
-                            </span>
-                          </label>
-                        </div>
-                      </td>
-                      <td>
-                        Flooded: One year later, assessing what was lost and
-                        what was found when a ravaging rain swept through metro
-                        Detroit
-                      </td>
-                      <td className='td-actions text-right'>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Edit Task'
-                          className='btn btn-primary btn-link btn-sm'>
-                          <i className='material-icons'>edit</i>
-                        </button>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Remove'
-                          className='btn btn-danger btn-link btn-sm'>
-                          <i className='material-icons'>close</i>
-                        </button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div className='form-check'>
-                          <label className='form-check-label'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              value=''
-                            />
-                            <span className='form-check-sign'>
-                              <span className='check' />
-                            </span>
-                          </label>
-                        </div>
-                      </td>
-                      <td>
-                        Sign contract for "What are conference organizers afraid
-                        of?"
-                      </td>
-                      <td className='td-actions text-right'>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Edit Task'
-                          className='btn btn-primary btn-link btn-sm'>
-                          <i className='material-icons'>edit</i>
-                        </button>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Remove'
-                          className='btn btn-danger btn-link btn-sm'>
-                          <i className='material-icons'>close</i>
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className='tab-pane' id='settings'>
-                <table className='table'>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <div className='form-check'>
-                          <label className='form-check-label'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              value=''
-                            />
-                            <span className='form-check-sign'>
-                              <span className='check' />
-                            </span>
-                          </label>
-                        </div>
-                      </td>
-                      <td>
-                        Lines From Great Russian Literature? Or E-mails From My
-                        Boss?
-                      </td>
-                      <td className='td-actions text-right'>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Edit Task'
-                          className='btn btn-primary btn-link btn-sm'>
-                          <i className='material-icons'>edit</i>
-                        </button>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Remove'
-                          className='btn btn-danger btn-link btn-sm'>
-                          <i className='material-icons'>close</i>
-                        </button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div className='form-check'>
-                          <label className='form-check-label'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              value=''
-                              checked
-                            />
-                            <span className='form-check-sign'>
-                              <span className='check' />
-                            </span>
-                          </label>
-                        </div>
-                      </td>
-                      <td>
-                        Flooded: One year later, assessing what was lost and
-                        what was found when a ravaging rain swept through metro
-                        Detroit
-                      </td>
-                      <td className='td-actions text-right'>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Edit Task'
-                          className='btn btn-primary btn-link btn-sm'>
-                          <i className='material-icons'>edit</i>
-                        </button>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Remove'
-                          className='btn btn-danger btn-link btn-sm'>
-                          <i className='material-icons'>close</i>
-                        </button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div className='form-check'>
-                          <label className='form-check-label'>
-                            <input
-                              className='form-check-input'
-                              type='checkbox'
-                              value=''
-                              checked
-                            />
-                            <span className='form-check-sign'>
-                              <span className='check' />
-                            </span>
-                          </label>
-                        </div>
-                      </td>
-                      <td>
-                        Sign contract for "What are conference organizers afraid
-                        of?"
-                      </td>
-                      <td className='td-actions text-right'>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Edit Task'
-                          className='btn btn-primary btn-link btn-sm'>
-                          <i className='material-icons'>edit</i>
-                        </button>
-                        <button
-                          type='button'
-                          rel='tooltip'
-                          title='Remove'
-                          className='btn btn-danger btn-link btn-sm'>
-                          <i className='material-icons'>close</i>
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+            <div className='card-body'>
+              <div className='tab-content'>
+                <MemberTask projectTasks={projectTasks} />
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 }
