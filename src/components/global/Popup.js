d48a66ab1dac8f383af3cff90427d7df5dab1a2a
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-07-22 11:59:32 +0200

    working popups in middle of screen

--- src/components/global/Popup.js
+++ src/components/global/Popup.js
@@ -5,12 +5,14 @@ import './Popup.css';
 export default function Popup(props) {
   const {error} = props;
   return (
-    <div className='alert alert-warning' role='alert'>
+    <div
+      className='alert alert-warning alert-dismissible fade show popup'
+      role='alert'>
       <div className='popupWrapper'>
         {error}
         <button
           type='button'
-          className='close'
+          className='close close-alert'
           data-dismiss='alert'
           aria-label='Close'>
           <span aria-hidden='true'>&times;</span>
