37dcc4f576f955badb670072253677d01649cd69
laura-mso <laura.socha@gmx.net>
2019-07-24 15:51:32 +0200

    Task edit status

--- src/components/task/TasksTable.js
+++ src/components/task/TasksTable.js
@@ -162,7 +162,9 @@ export default class TasksTable extends React.Component {
         ) : (
           Object.keys(tasksByProjects).map((key, index) => (
             <ActionsTable
-              entities={this.state.tasks}
+              entities={this.state.tasks.filter(currentElement => {
+                return currentElement.projectId === key;
+              })}
               tableName={
                 key
                   ? findInArray(this.state.projects, 'id', key, 'title')
--- src/util/config.js
+++ src/util/config.js
@@ -1,1 +1,1 @@
-export const config = {apiUrl: 'http://localhost:3000/api/v1'};
+export const config = {apiUrl: 'http://lesewert.herokuapp.com/api/v1'};
